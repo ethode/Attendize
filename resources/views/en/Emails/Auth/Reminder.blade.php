24cb31825c08e77d5816767097750b57a996b25f
Tyler Schoen <tyler@ethode.com>
2018-08-22 15:49:40 -0400

    Branding/styling updates

--- resources/views/en/Emails/Auth/Reminder.blade.php
+++ resources/views/en/Emails/Auth/Reminder.blade.php
@@ -6,6 +6,6 @@
         To reset your password, complete this form: {{ route('showResetPassword', ['token' => $token]) }}.
         <br><br><br>
         Thank you,<br>
-        Team Attendize
+        Team Blockland
     </div>
 @stop
